d0059ff09d91a53ba15c9fd7cd0794c704dbc335
Shashith-Malshan <malshanshashith318@gmail.com>
2025-12-04 13:04:23 +0530

    Create Function getTimeStamp

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
 console.log("connected");
+
+const chatBox = document.getElementById("chatBox");
+const userInput = document.getElementById("userInput");
+const sendBtn = document.getElementById("sendBtn");
+
+function getTimestamp() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+}
